1ea060ae45ad52dac28c8487e371c91b5b0ef682
Mike Eng <mike@buildium.com>
2013-11-26 21:39:12 -0500

    removing boilerplate, adding mikeflake

--- js/ecard.js
+++ js/ecard.js
@@ -1,16 +1,26 @@
 $( document ).ready(function() {
-	var x = 100;
-	var y = -141;
-	var dx = 0;
-	var dy = 2;
-	var w = 306;
-	var h = 299;
 	var ctx;
 	var WIDTH;
 	var HEIGHT;
 	var intervalId = 0;
 	var emflake = new Image();
 	emflake.src = 'img/emflake.png';
+	var x = 100;
+	var y = -141;
+	var dx = 0;
+	var dy = 2;
+	var w = 306;
+	var h = 299;
+	
+	var mikeflake = new Image();
+	mikeflake.src = 'img/mikeflake.png';
+	var mx = 300;
+	var my = -641;
+	var dmx = 0;
+	var dmy = 2;
+	var mw = 261;
+	var mh = 292;
+	
 	
 	function init(){
 		//set context
@@ -25,15 +35,23 @@ $( document ).ready(function() {
 	function draw(){
 		clear();
 		drawEmflake(x, y, w, h);
-		
-		dx = getRandomInt(-1,1);
-		
+		dx = getRandomInt(-1,1);		
 		x += dx;
 		y += dy;
+		
+		drawMikeflake(mx, my, mw, mh);
+		dmx = getRandomInt(-1,1);		
+		mx += dmx;
+		my += dmy;
+		
 	}
 	
 	function drawEmflake(x, y, w, h){
 		ctx.drawImage(emflake, x, y, w, h);
+	}
+	
+	function drawMikeflake(mx, my, mw, mh){
+		ctx.drawImage(mikeflake, mx, my, mw, mh);
 	}
 	
 	function clear() {
